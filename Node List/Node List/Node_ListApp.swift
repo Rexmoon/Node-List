0484a8fee2a8db7cba84748a447398403b023829
rexmoon <lunajose6969@gmail.com>
2024-02-09 16:48:39 -0600

    feat: Project created

---
+++ Node List/Node List/Node_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Node_ListApp.swift
+//  Node List
+//
+//  Created by Bryan Luna on 2/9/24.
+//
+
+import SwiftUI
+
+@main
+struct Node_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
